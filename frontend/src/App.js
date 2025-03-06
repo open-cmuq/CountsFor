f7b0595aa6d9ce77f0cc503ecac3744822cc555e
jullia02 <jmontejo@andrew.cmu.edu>
2025-03-06 12:09:33 +0300

    FEAT: added main page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,13 @@
-import './App.css';
+// import CourseTableMock from "./components/courseTableMock/courseTableMock.js";
+import CourseTableMock from "./components/CourseTableMock";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+      <CourseTableMock />
     </div>
   );
 }
 
 export default App;
+
